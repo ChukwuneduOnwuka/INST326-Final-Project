0c15d884b6e9efca1a8347364b9fa77199c02f65
Chinedu Onwuka <114266464+ChukwuneduOnwuka@users.noreply.github.com>
2023-04-14 14:40:23 -0400

    Add files via upload
    very basic starting point

---
+++ Class Registration.py
@@ -0,0 +1,13 @@
+class Student:
+     """Contains information for students.
+    
+    Attributes:
+        Student_ID (int): ID number of student
+    """
+
+class Course:
+    """Contains information for courses.
+    
+    Attributes:
+        Course_Name (str): Name of course
+    """
