6f0485efc059c9342d255b1b956f2490db387f7c
Samarth Motwani <smotwani@terpmail.umd.edu>
2023-05-01 10:43:59 -0400

    updated student_registration.py

--- student_registration.py
+++ student_registration.py
@@ -1,3 +1,19 @@
+import re
+UMD_MAJORS = {
+    'A. James Clark School of Engineering': ['Aerospace Engineering', 'Chemical and Biomolecular Engineering', 'Civil and Environmental Engineering', 'Computer Engineering', 'Electrical Engineering', 'Materials Science and Engineering', 'Mechanical Engineering'],
+    'College of Agriculture and Natural Resources': ['Agricultural and Resource Economics', 'Animal Sciences', 'Environmental Science and Policy', 'Plant Science'],
+    'College of Arts and Humanities': ['Art History', 'Classical Languages and Literatures', 'Communication', 'Dance', 'English Language and Literature', 'Film Studies', 'French Language and Literature', 'Germanic Studies', 'Linguistics', 'Music', 'Philosophy', 'Spanish Language and Literature', 'Theatre', 'Women\'s Studies'],
+    'College of Behavioral and Social Sciences': ['Anthropology', 'Criminology and Criminal Justice', 'Economics', 'Geographical Sciences', 'Government and Politics', 'Psychology', 'Sociology'],
+    'College of Computer, Mathematical, and Natural Sciences': ['Applied Mathematics and Statistics', 'Atmospheric and Oceanic Science', 'Biochemistry', 'Biological Sciences', 'Chemistry', 'Computer Science', 'Geology', 'Mathematics', 'Neuroscience and Cognitive Science', 'Physics', 'Physiology and Neurobiology', 'Statistics'],
+    'College of Education': ['Education', 'Human Development and Quantitative Methodology', 'Special Education'],
+    'College of Information Studies': ['Information Science'],
+    'Philip Merrill College of Journalism': ['Journalism'],
+    'Robert H. Smith School of Business': ['Accounting', 'Business Management', 'Finance', 'Management Information Systems', 'Marketing', 'Supply Chain Management'],
+    'School of Architecture, Planning and Preservation': ['Architecture'],
+    'School of Public Health': ['Health Administration and Policy', 'Public Health Science'],
+    'School of Nursing': ['Nursing']
+}
+
 class Student:
     """Class that repesents student information
     Attributes:
@@ -22,6 +38,20 @@ class Student:
         
         
         """
+        if not isinstance(student_id, int):
+            return TypeError
+        
+        if not isinstance(phone, int):
+            return TypeError
+        
+        id_match = re.match(r'^117\d{5}$', student_id)
+        phone_match =  re.match(r'^\d{10}$' ,phone)
+        
+        if not id_match:
+            raise ValueError("Invalid student ID")
+        if not phone_match:
+            raise ValueError("Invalud phone number")
+        
         
         self.student_id = student_id
         self.name = name
